fa9f8c691f8477eec0068968b991c05c87cedca6
yessine <yessine.agrebi.aiesec@gmail.com>
2023-10-07 20:38:28 +0200

    workout exceptions handlers

--- src/workouts/workouts.controller.ts
+++ src/workouts/workouts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, BadRequestException, NotFoundException } from '@nestjs/common';
 import { WorkoutsService } from './workouts.service';
 import { CreateWorkoutDto } from './dto/create-workout.dto';
 import { UpdateWorkoutDto } from './dto/update-workout.dto';
@@ -8,27 +8,47 @@ export class WorkoutsController {
   constructor(private readonly workoutsService: WorkoutsService) {}
 
   @Post()
-  create(@Body() createWorkoutDto: CreateWorkoutDto) {
-    return this.workoutsService.create(createWorkoutDto);
+  async create(@Body() createWorkoutDto: CreateWorkoutDto) {
+    const workout = await this.workoutsService.create(createWorkoutDto);
+    if (!workout) {
+      throw new BadRequestException('Error creating workout');
+    }
+    return workout;
   }
 
   @Get()
-  findAll() {
-    return this.workoutsService.findAll();
+  async findAll() {
+    const workouts = await this.workoutsService.findAll();
+    if (!workouts) {
+      throw new NotFoundException('Error finding workouts');
+    }
+    return workouts;
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.workoutsService.findOne(+id);
+  async findOne(@Param('id') id: string) {
+    const workout = await this.workoutsService.findOne(+id);
+    if (!workout) {
+      throw new NotFoundException(`Error finding workout with id ${id}`);
+    }
+    return workout;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateWorkoutDto: UpdateWorkoutDto) {
-    return this.workoutsService.update(+id, updateWorkoutDto);
+  async update(@Param('id') id: string, @Body() updateWorkoutDto: UpdateWorkoutDto) {
+    const workout = await this.workoutsService.update(+id, updateWorkoutDto);
+    if (!workout) {
+      throw new NotFoundException(`Error updating workout with id ${id}`);
+    }
+    return workout;
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.workoutsService.remove(+id);
+  async remove(@Param('id') id: string) {
+    const workout = await this.workoutsService.remove(+id);
+    if (!workout) {
+      throw new NotFoundException(`Error deleting workout with id ${id}`);
+    }
+    return workout;
   }
 }
--- src/workouts/workouts.service.ts
+++ src/workouts/workouts.service.ts
@@ -6,23 +6,23 @@ import { PrismaService } from 'src/prisma/prisma.service';
 @Injectable()
 export class WorkoutsService {
   constructor(private prisma: PrismaService) {}
-  async create(createWorkoutDto: CreateWorkoutDto) {
-    return await this.prisma.workout.create({data: createWorkoutDto})
+  create(createWorkoutDto: CreateWorkoutDto) {
+    return this.prisma.workout.create({data: createWorkoutDto})
   }
 
-  async findAll() {
+  findAll() {
     return this.prisma.workout.findMany()
   }
 
-  async findOne(id: number) {
+  findOne(id: number) {
     return this.prisma.workout.findUnique({where: {workoutId: id}})
   }
 
-  async update(id: number, updateWorkoutDto: UpdateWorkoutDto) {
+  update(id: number, updateWorkoutDto: UpdateWorkoutDto) {
     return this.prisma.workout.update({where: {workoutId: id}, data: updateWorkoutDto})
   }
 
-  async remove(id: number) {
+  remove(id: number) {
     return this.prisma.workout.delete({where: {workoutId: id}})
   }
 }
