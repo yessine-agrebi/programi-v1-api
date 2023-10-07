1e030460d66b29bb32aebb3030d6c2015d5c8674
yessine <yessine.agrebi.aiesec@gmail.com>
2023-10-07 20:24:20 +0200

    exercise exception handler

--- src/exercises/exercises.controller.ts
+++ src/exercises/exercises.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  BadRequestException,
+  NotFoundException,
+} from '@nestjs/common';
 import { ExercisesService } from './exercises.service';
 import { CreateExerciseDto } from './dto/create-exercise.dto';
 import { UpdateExerciseDto } from './dto/update-exercise.dto';
@@ -8,28 +18,50 @@ export class ExercisesController {
   constructor(private readonly exercisesService: ExercisesService) {}
 
   @Post()
-  create(@Body() createExerciseDto: CreateExerciseDto) {
-    return this.exercisesService.create(createExerciseDto);
+  async create(@Body() createExerciseDto: CreateExerciseDto) {
+    const exercise = await this.exercisesService.create(createExerciseDto);
+    if (!exercise) {
+      throw new BadRequestException(`Error creating exercise`);
+    }
+    return exercise;
   }
 
   @Get()
   findAll() {
-    return this.exercisesService.findAll();
+    const exercises = this.exercisesService.findAll();
+    if (!exercises) {
+      throw new NotFoundException(`Error finding exercises`);
+    }
+    return exercises;
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    console.log(id)    
-    return this.exercisesService.findOne(+id);
+    const exercise = this.exercisesService.findOne(+id);
+    if (!exercise) {
+      throw new NotFoundException(`Error finding exercise with id ${id}`);
+    }
+    return exercise;
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateExerciseDto: UpdateExerciseDto) {
-    return this.exercisesService.update(+id, updateExerciseDto);
+  update(
+    @Param('id') id: string,
+    @Body() updateExerciseDto: UpdateExerciseDto,
+  ) {
+    const exercise = this.exercisesService.update(+id, updateExerciseDto);
+    if (!exercise) {
+      throw new NotFoundException(`Error updating exercise with id ${id}`);
+    }
+    return exercise;
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.exercisesService.remove(+id);
+    const exercise = this.exercisesService.remove(+id);
+    if (!exercise) {
+      throw new NotFoundException(`Error deleting exercise with id ${id}`);
+    }
+    return exercise;
   }
 }
--- src/exercises/exercises.service.ts
+++ src/exercises/exercises.service.ts
@@ -6,32 +6,24 @@ import { CreateExerciseDto } from './dto/create-exercise.dto';
 @Injectable()
 export class ExercisesService {
   constructor(private readonly prisma: PrismaService) {}
-  async create(createExerciseDto: CreateExerciseDto) {
-    try {
-      const createdExercise = await this.prisma.exercise.create({
-        data: createExerciseDto
-      });
-      return createdExercise;
-    } catch (error) {
-      console.error('Error creating exercise:', error);
-      throw error; // Re-throw the error for higher-level handling
-    }
+  create(createExerciseDto: CreateExerciseDto) {
+    return this.prisma.exercise.create({ data: createExerciseDto });
   }
   
 
-  async findAll() {
-    return await this.prisma.exercise.findMany();
+  findAll() {
+    return this.prisma.exercise.findMany();
   }
 
-  async findOne(id: number) {
-    return await this.prisma.exercise.findUnique({ where: { exerciseId: id } });
+  findOne(id: number) {
+    return this.prisma.exercise.findUnique({ where: { exerciseId: id } });
   }
 
-  async update(id: number, updateExerciseDto: UpdateExerciseDto) {
-    return await this.prisma.exercise.update({ where: { exerciseId: id }, data: updateExerciseDto });
+  update(id: number, updateExerciseDto: UpdateExerciseDto) {
+    return this.prisma.exercise.update({ where: { exerciseId: id }, data: updateExerciseDto });
   }
 
-  async remove(id: number) {
-    return await this.prisma.exercise.delete({ where: { exerciseId: id } });
+  remove(id: number) {
+    return this.prisma.exercise.delete({ where: { exerciseId: id } });
   }
 }
