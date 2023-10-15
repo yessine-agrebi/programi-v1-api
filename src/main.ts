28d5df3b6602ca1042f0ef1f652f84ba678d12fd
rami chabchoub <ramichabchoub.rc@gmail.com>
2023-10-15 10:56:18 +0100

    Style: setup cookie-session package

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+let CookieSession: any;
+
+console.log(process.env.SESSION_SECRET);
+
 async function bootstrap() {
+  CookieSession = await import('cookie-session');
   const app = await NestFactory.create(AppModule);
+  app.use(CookieSession({ keys: [process.env.SESSION_SECRET] }));
   app.enableCors({ origin: 'http://localhost:3000' });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(5000);
