32a5fdfc92a332ea49651476676effbea2e0b84e
rami chabchoub <ramichabchoub.rc@gmail.com>
2023-10-15 07:41:10 +0100

    Perf: no need for check user in auth service(already in user service)

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
@@ -11,12 +11,6 @@ export class AuthService {
   constructor(private usersService: UsersService) {}
 
   async signup(attributes: Partial<User>) {
-    const existingUser = await this.usersService.findOneByEmail(
-      attributes.email,
-    );
-    if (existingUser) {
-      throw new ConflictException('User already exists');
-    }
     const salt = randomBytes(8).toString('hex');
     const hash = (await scrypt(attributes.password, salt, 32)) as Buffer;
     const result = salt + '.' + hash.toString('hex');
